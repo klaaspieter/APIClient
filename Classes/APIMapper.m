cb012fe65ad6e06bcee2b83fa9c396eb4f34da85
Klaas Pieter Annema <klaaspieter@annema.me>
2013-10-19 08:40:56 -0400

    Use DCKeyValueObjectMapping for mapping

--- Classes/APIMapper.m
+++ Classes/APIMapper.m
@@ -8,11 +8,19 @@
 
 #import "APIMapper.h"
 
+#import "DCKeyValueObjectMapping.h"
+
 @implementation APIMapper
 
 - (id)mapValues:(id)values toResource:(Class)resource;
 {
-    return nil;
+    DCKeyValueObjectMapping *parser = [DCKeyValueObjectMapping mapperForClass:resource];
+
+    if ([values isKindOfClass:[NSArray class]]) {
+        return [parser parseArray:values];
+    } else {
+        return [parser parseDictionary:values];
+    }
 }
 
 @end
